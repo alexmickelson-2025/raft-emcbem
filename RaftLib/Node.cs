cc10608f2bfe446663f23fbdf3c5e281490a7c5f
ethanenger <engerethan@gmail.com>
2025-01-23 18:15:58 -0700

    15 IS DONE

--- RaftLib/Node.cs
+++ RaftLib/Node.cs
@@ -4,12 +4,13 @@
 public class Node : INode
 {
     public NodeState CurrentState { get; set; } = NodeState.Follower;
-    public Dictionary<int, int> OtherNextIndexes { get; set; } = new();
     public int CurrentTerm { get; set; } = 0;
     public System.Timers.Timer? InternalTimer { get; set; }
+    public Dictionary<int, int> OtherNextIndexes { get; set; } = new();
     public Dictionary<int, int> WhoDidIVoteFor { get; set; } = new();
     public Dictionary<int, int> CurrentVotesForTerm { get; set; } = new();
     public Dictionary<int, int> LogReplicated { get; set; } = new();
+    public Dictionary<string, string> InternalStateMachine {get; set;} = new();
     public int Id { get; set; }
     public INode[] nodes { get; set; } = [];
     public int Majority { get => ((nodes.Count() + 1) / 2) + 1; }
@@ -17,12 +18,11 @@ public class Node : INode
     public DateTime StartTime;
     public double TimerInterval = 0;
     public List<Log> LogList { get; set; } = new();
-    public int NextIndex { get => LogList.Count + 1; }
+    public int NextIndex { get => LogList.Count; }
     public int MinInterval { get; set; } = 150;
     public int MaxInterval { get; set; } = 301;
     public int HeartbeatInterval { get; set; } = 50;
     public int CommitIndex { get; set; }
-    public Dictionary<string, string> InternalStateMachine {get; set;} = new();
 
 
     public Node(int idNum)
@@ -94,7 +94,7 @@ public class Node : INode
     {
         foreach (var node in nodes)
         {
-            OtherNextIndexes[node.Id] = NextIndex;
+            OtherNextIndexes[node.Id] = NextIndex + 1;
         }
     }
 
@@ -122,7 +122,7 @@ public class Node : INode
     private Log[] GetOtherNodesLogList(int nodeId)
     {
         var nodesNextIndex = OtherNextIndexes[nodeId];
-        var logDifference = NextIndex - nodesNextIndex;
+        var logDifference = NextIndex + 1 - nodesNextIndex;
 
         if (logDifference < 0)
         {
@@ -143,7 +143,7 @@ public class Node : INode
     public async Task RequestVoteRPC(int candidateId, int termToVoteFor)
     {
         bool result = false;
-        if (!WhoDidIVoteFor.TryGetValue(termToVoteFor, out _) && termToVoteFor > CurrentTerm)
+        if (!WhoDidIVoteFor.ContainsKey(termToVoteFor) && termToVoteFor > CurrentTerm)
         {
             WhoDidIVoteFor.Add(termToVoteFor, candidateId);
             result = true;
@@ -165,18 +165,13 @@ public class Node : INode
         }
     }
 
-    private async Task SendAppendResponse(int leaderId, int term)
+    private async Task SendAppendResponse(int leaderId, bool response)
     {
         var nodeToRespondTo = nodes.Where(x => x.Id == leaderId).FirstOrDefault();
 
         if (nodeToRespondTo != null)
         {
-            if (term < CurrentTerm)
-            {
-                await nodeToRespondTo.ResponseAppendLogRPC(false, Id, CurrentTerm, LogList.Count());
-                return;
-            }
-            await nodeToRespondTo.ResponseAppendLogRPC(true, Id, CurrentTerm,  LogList.Count());
+            await nodeToRespondTo.ResponseAppendLogRPC(response, Id, CurrentTerm,  NextIndex);
         }
     }
 
@@ -203,9 +198,13 @@ public class Node : INode
                 LogReplicated[index]++;
                 if (LogReplicated[index] >= Majority)
                 {
-                    CommitLog(index - 1);
+                    CommitLog(index);
                 }
             }
+        }
+        else
+        {
+            OtherNextIndexes[id]--;
         }
         await Task.CompletedTask;
     }
@@ -253,11 +252,44 @@ public class Node : INode
 
             if (NodeState.Leader != CurrentState)
             {
-                LogList.AddRange(entries);
                 StartNewCanidacyTimer();
             }
         }
-        await SendAppendResponse(leaderId, term);
+        AddOrRemoveLogs(leaderId, entries, commitIndex, prevIndex, prevTerm);
+        await SendAppendResponse(leaderId, DetermineResponse(term,commitIndex, prevIndex, prevTerm));
+    }
+
+    private void AddOrRemoveLogs(int leaderId, Log[] entries, int commitIndex, int prevIndex, int prevTerm)
+    {
+        var ourPrevTerm = NextIndex > 0 ? LogList[NextIndex - 1].Term : 0;
+        if(NextIndex != prevIndex || prevTerm != ourPrevTerm)
+        {
+            if (LogList.Count > prevIndex )
+            {
+                LogList.RemoveRange(prevIndex, LogList.Count - (prevIndex));
+            }
+        }
+        else
+        {
+            LogList.AddRange(entries);
+        }
+    }
+
+    private bool DetermineResponse(int term, int commitIndex, int prevIndex, int prevTerm)
+    {
+        if(term < CurrentTerm)
+        {
+            return false;
+        }
+        var ourPrevIndex = NextIndex ;
+        var ourPrevTerm = NextIndex > 0 ? LogList[NextIndex - 1].Term : 0;
+        var response = true;
+
+        if(prevIndex != ourPrevIndex || ourPrevTerm != prevTerm)
+        {
+            response =  false;
+        }
+        return response;
     }
 
     public void StopTimer()
--- RaftTests/LoggingTests.cs
+++ RaftTests/LoggingTests.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using FluentAssertions;
 using NSubstitute;
+using NSubstitute.Routing.AutoValues;
 using RaftLib;
 
 namespace RaftTests;
@@ -19,11 +20,11 @@ public class LoggingTests
         leaderNode.ReceiveClientRequest("test1", "test2");
         await Task.Delay(75);
 
-       await moqNode.Received().RequestAppendLogRPC(1, 0, Arg.Is<Log[]>(logs =>
-            logs.Length == 1 &&
-            logs[0].Equals(new Log(0, "test1", "test2"))
-        ), 0, 1, 0);
-        }
+        await moqNode.Received().RequestAppendLogRPC(1, 0, Arg.Is<Log[]>(logs =>
+             logs.Length == 1 &&
+             logs[0].Equals(new Log(0, "test1", "test2"))
+         ), 0, 1, 0);
+    }
 
 
     // Testing #2
@@ -93,8 +94,8 @@ public class LoggingTests
 
         Thread.Sleep(75);
 
-        moqNode1.Received().RequestAppendLogRPC(0, 0, Arg.Is<Log[]>(logs => logs.Count() == 2 && logs[0].Equals(log1) && logs[1].Equals(log2) ), 0, 2, 0);
-        moqNode2.Received().RequestAppendLogRPC(0, 0, Arg.Is<Log[]>(logs => logs.Count() == 1 && logs[0].Equals(log2) ), 0, 2, 0);
+        moqNode1.Received().RequestAppendLogRPC(0, 0, Arg.Is<Log[]>(logs => logs.Count() == 2 && logs[0].Equals(log1) && logs[1].Equals(log2)), 0, 2, 0);
+        moqNode2.Received().RequestAppendLogRPC(0, 0, Arg.Is<Log[]>(logs => logs.Count() == 1 && logs[0].Equals(log2)), 0, 2, 0);
     }
 
     // Testing #6 
@@ -109,7 +110,7 @@ public class LoggingTests
         node.InitiateLeadership();
         await moqNode1.Received().RequestAppendLogRPC(1, 0, Arg.Any<Log[]>(), 0, 0, 0);
         node.ReceiveClientRequest("test", "log");
-        await node.ResponseAppendLogRPC(true, 1, 0, 1);
+        await node.ResponseAppendLogRPC(true, 1, 0, 0);
         await Task.Delay(75);
 
         //Assert
@@ -122,11 +123,11 @@ public class LoggingTests
     {
         // Given
         var node = new Node(1);
-    
+
         // When
         node.InitiateLeadership();
         node.ReceiveClientRequest("hi", "there");
-    
+
         // Then
         node.InternalStateMachine["hi"].Should().Be("there");
         node.CommitIndex.Should().Be(1);
@@ -138,17 +139,17 @@ public class LoggingTests
     {
         // Given
         var node = new Node(1, TestNode.LargeCluster);
-    
+
         // When
         node.InitiateLeadership();
         node.ReceiveClientRequest("hi", "there");
         node.InternalStateMachine.ContainsKey("hi").Should().BeFalse();
 
-        await node.ResponseAppendLogRPC(true, 2, 0, 1);
+        await node.ResponseAppendLogRPC(true, 2, 0, 0);
         node.InternalStateMachine.ContainsKey("hi").Should().BeFalse();
 
-        await node.ResponseAppendLogRPC(true, 2, 0, 1);
-    
+        await node.ResponseAppendLogRPC(true, 2, 0, 0);
+
         // Then
         node.InternalStateMachine.ContainsKey("hi").Should().BeTrue();
         node.InternalStateMachine["hi"].Should().Be("there");
@@ -158,28 +159,28 @@ public class LoggingTests
     [Fact]
     public void GivenALeaderNodeWhenTheyRecieveAClientRequestAndTheyAreTheOnlyNodeInTheClusterItIsCommitedAndTheIndexGoesUp()
     {
-        var node =  new Node(1, []);
-    
-        node.ReceiveClientRequest("hi", "there");    
+        var node = new Node(1, []);
 
-        node.CommitIndex.Should().Be(1);  
+        node.ReceiveClientRequest("hi", "there");
+
+        node.CommitIndex.Should().Be(1);
     }
 
     // Testing #9.b
     [Fact]
     public async Task GivenALeaderNodeWhenTheyRecieveAClientRequestAndTheClusterSizeIs5InThen2NodesRespondWithSuccessThenTheIndexGoesUp()
     {
-        var node =  new Node(1, TestNode.LargeCluster);
-    
+        var node = new Node(1, TestNode.LargeCluster);
+
         node.InitiateLeadership();
-        node.ReceiveClientRequest("hi", "there");    
-        node.CommitIndex.Should().Be(0);  
+        node.ReceiveClientRequest("hi", "there");
+        node.CommitIndex.Should().Be(0);
 
-        await node.ResponseAppendLogRPC(true, 2, 0, 1);
-        node.CommitIndex.Should().Be(0);  
+        await node.ResponseAppendLogRPC(true, 2, 0, 0);
+        node.CommitIndex.Should().Be(0);
 
-        await node.ResponseAppendLogRPC(true, 3, 0, 1);
-        node.CommitIndex.Should().Be(1);  
+        await node.ResponseAppendLogRPC(true, 3, 0, 0);
+        node.CommitIndex.Should().Be(1);
     }
 
     // Testing #10
@@ -190,9 +191,9 @@ public class LoggingTests
         var node = new Node();
         node.StopTimer();
         var LogToAdd = new Log(1, "Key", "Value");
-    
+
         // When
-        await node.RequestAppendLogRPC(2, 0, [LogToAdd], 0, 0, 0);
+        await node.RequestAppendLogRPC(2, 1, [LogToAdd], 0, 0, 0);
 
         // Then
         node.LogList.Should().Contain(LogToAdd);
@@ -206,11 +207,11 @@ public class LoggingTests
         moqLeader.Id = 2;
         var node = new Node(1, [moqLeader]);
 
-        await node.RequestAppendLogRPC(2, 0, [], 0, 0, 0);
-        await moqLeader.Received().ResponseAppendLogRPC(true, 1, 0, 0);
+        await node.RequestAppendLogRPC(2, 1, [], 0, 0, 0);
+        await moqLeader.Received().ResponseAppendLogRPC(true, 1, 1, 0);
 
-        await node.RequestAppendLogRPC(2, 0, [new Log(0, "blah", "blah")], 0, 0, 0);
-        await moqLeader.Received().ResponseAppendLogRPC(true, 1, 0, 1);
+        await node.RequestAppendLogRPC(2, 1, [new Log(0, "blah", "blah")], 0, 0, 0);
+        await moqLeader.Received().ResponseAppendLogRPC(true, 1, 1, 0);
     }
 
     // Testing #13
@@ -219,16 +220,16 @@ public class LoggingTests
     {
         // Given
         var node = new Node(1);
-    
+
         // When
         node.InitiateLeadership();
         node.ReceiveClientRequest("test", "machine");
-    
+
         // Then
         node.InternalStateMachine["test"].Should().Be("machine");
     }
 
-    // Testing #15
+    // Testing #15.a
     [Fact]
     public void GivenALeaderNodeWhenSendingHeartbeatsThenIncludeTheTermAndIndexOfTheLastLog()
     {
@@ -238,12 +239,69 @@ public class LoggingTests
         var leader = new Node(1, [moqFollower]);
         leader.InitiateLeadership();
         moqFollower.Received().RequestAppendLogRPC(1, 0, [], 0, 0, 0);
-    
+
         // When
-        leader.ReceiveClientRequest("test","test");
+        leader.ReceiveClientRequest("test", "test");
         Thread.Sleep(75);
-    
+
         // Then
         moqFollower.Received().RequestAppendLogRPC(1, 0, Arg.Any<Log[]>(), 0, 1, 0);
+    }
+
+    // Testing 15.b
+    [Fact]
+    public async Task GivenAFollowerNodeWhenThePrevIndexOfAnAppendEntriesRPCDoesNotMatchTheInternalLogThenItRespondsWithFalse()
+    {
+        // Given
+        var moqLeader = Substitute.For<INode>();
+        moqLeader.Id = 2;
+        var node = new Node(1, [moqLeader]);
+        node.StopTimer();
+
+        // When
+        await node.RequestAppendLogRPC(2, 0, [], 0, 1, 0);
+
+        // Then
+        await moqLeader.Received().ResponseAppendLogRPC(false, Arg.Any<int>(), Arg.Any<int>(), Arg.Any<int>());
+    }
+
+    // Testing 15.c
+    [Fact]
+    public async Task GivenAFollowerNodeWhenTheyHaveOutOfDateLogsAndALeaderTellsThemToAppendALogAtALowerIndexTheyDeleteEverythingAboveWhereTheyNeedToAndRespondFalse()
+    {
+        // Given
+        var moqLeader = Substitute.For<INode>();
+        moqLeader.Id = 2;
+        var node = new Node(1, [moqLeader]);
+        node.StopTimer();
+        node.LogList = [new Log(0, "Im", "Outdated")];
+
+        // When
+        await node.RequestAppendLogRPC(2, 1, [], 0, 0, 0);
+    
+        // Then
+        node.LogList.Should().BeEmpty();
+        await moqLeader.Received().ResponseAppendLogRPC(true, 1, 1, 0);
+    }
+
+    // Testing 15.d
+    [Fact]
+    public async Task GivenALeaderWhenItRecievesAFailingResponseItDecrementsThenNextIndexIsChangedInTheDict()
+    {
+        // Given
+        var moqFollower = Substitute.For<INode>();
+        moqFollower.Id = 1;
+        var leaderNode = new Node(2, [moqFollower]);
+        leaderNode.ReceiveClientRequest("Hi", "There");
+        leaderNode.ReceiveClientRequest("Hi2", "There2");
+        leaderNode.InitiateLeadership();
+
+        // When
+        leaderNode.OtherNextIndexes[1].Should().Be(3);
+        await leaderNode.ResponseAppendLogRPC(false, 1, 2, 0);
+
+        // Then
+        leaderNode.OtherNextIndexes[1].Should().Be(2);
+
     }
 }
